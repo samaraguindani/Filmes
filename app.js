9c642763fdf5ca614346454207d7c50088fbf5eb
Samara Guindani <samara.guindani@universo.univates.com>
2024-03-08 14:38:54 -0300

    favoritar

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ import { apiKey } from "./key.js";
 const filmes = document.querySelector(".filmes");
 const input = document.querySelector(".container-pesquisa");
 const botaoSearch = document.querySelector(".container-pesquisa-icone");
+var checkbox = document.querySelector(".container-checkbox");
 
 botaoSearch.addEventListener('click', searchMovie);
+
+checkbox.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("Checkbox is checked..");
+  } else {
+    console.log("Checkbox is not checked..");
+  }
+});
 
 input.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
@@ -29,7 +38,7 @@ async function searchMovie() {
       const movies = await searchMovieByName(inputValue)
       movies.forEach(movie => renderMovie(movie))
     }
-  }
+}
 
 async function searchMovieByName(title) {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${title}&language=en-US&page=1`
@@ -49,9 +58,47 @@ function cleanAllMovies() {
     filmes.innerHTML = ''
 }
 
+function favoriteButtonPressed(event, movie) {
+    const favoriteState = {
+        favoritado: 'image/Vector.svg',
+        desfavoritado: 'image/Heart.svg'
+    };
+
+    if(event.target.src.includes(favoriteState.desfavoritado)) {
+        event.target.src = favoriteState.favoritado;
+        saveToLocalStorage(movie);
+    } else {
+        event.target.src = favoriteState.desfavoritado;
+        removeFromLocalStorage(movie.id);
+    }
+}
+
+function getFavoriteMovies() {
+    return JSON.parse(localStorage.getItem('favoriteMovies'));
+}
+
+function saveToLocalStorage(movie) {
+    const movies = getFavoriteMovies() || [];
+    movies.push(movie);
+    const filmesJSON = JSON.stringify(movies);
+    localStorage.setItem('favoriteMovies', filmesJSON);
+}
+
+function checkMovieIsFavorited(id) {
+    const movies = getFavoriteMovies() || [];
+    return movies.find(movie => movie.id == id);
+}
+
+function removeFromLocalStorage(id) {
+    const movies = getFavoriteMovies() || [];
+    const encontraFilme = movies.find(movie => movie.id == id);
+    const filmesNovos = movies.filter(movie => movie.id != encontraFilme.id);
+    localStorage.setItem('favoriteMovies', JSON.stringify(filmesNovos));
+}
+
 function renderMovie(movie) {
-    const { title, poster_path, vote_average, release_date, overview } = movie;
-    const isFavorited = false;
+    const { id, title, poster_path, vote_average, release_date, overview } = movie;
+    const isFavorited = checkMovieIsFavorited(id);
     const year = new Date(release_date).getFullYear();
     const image = `https://image.tmdb.org/t/p/w500${poster_path}`;
 
@@ -94,17 +141,15 @@ function renderMovie(movie) {
 
     const divFilmeFav = document.createElement('div');
     divFilmeFav.classList.add('filme__fav');
-    const buttonFilmeFav = document.createElement('button');
-    buttonFilmeFav.classList.add('img-coracao');
     const imagemCoracao = document.createElement('img');
     imagemCoracao.src = isFavorited ? 'image/Vector.svg' : 'image/Heart.svg';  //se true coracao preechido se nao coracao aberto
     imagemCoracao.alt = 'Heart';
+    imagemCoracao.addEventListener('click', (event) => favoriteButtonPressed(event, movie));
     imagemCoracao.classList.add('img-coracao');
     const favoritoTexto = document.createElement('span');
     favoritoTexto.classList.add('avaliacao-paragrafo');
     favoritoTexto.textContent = 'Favoritar';
-    buttonFilmeFav.appendChild(imagemCoracao);
-    divFilmeFav.appendChild(buttonFilmeFav);
+    divFilmeFav.appendChild(imagemCoracao);
     divFilmeFav.appendChild(favoritoTexto);
     divFilmeAvaliacao.appendChild(divFilmeFav);
 
@@ -116,4 +161,3 @@ function renderMovie(movie) {
     divFilmeSinopse.appendChild(filmeSinopse);
     divFilme.appendChild(divFilmeSinopse);
 }
-
